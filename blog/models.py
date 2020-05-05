9f8c7023282ad2e75f6e21e191e19a613e4e6506
jemajet <mark6000512@gmail.com>
2020-05-05 02:55:38 -0400

    Added bootstrap to show jobs on homepage

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    # Blog has a title, a publication date, an image, and a body
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- jobs/models.py
+++ jobs/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Job(models.Model):
-    # Job has an image of the job and a summary
+    # Job has an image of the job, a summary, and a title
+    title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
     summary = models.CharField(max_length=200)
