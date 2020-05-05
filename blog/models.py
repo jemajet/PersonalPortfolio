a5b42424a13fd42cfe9e364ba3b06478327b8265
jemajet <mark6000512@gmail.com>
2020-05-05 04:31:12 -0400

    Blog functionality added, and website now looks good and navbar works, as well as blog buttons word

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,15 @@ class Blog(models.Model):
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+
+    def summary(self):
+        if len(self.body) < 100:
+            return self.body
+        else:
+            return self.body[:100] + "... Read More"
+
+    def pub_date_pretty(self):
+        return self.pub_date.strftime("%b %e %Y")
+
+    def __str__(self):
+        return self.title
