4fe48021412ebd947e3a897522bea3f0752fe82b
jemajet <mark6000512@gmail.com>
2020-05-21 00:57:12 -0400

    Updating blog for deployment

--- jobs/models.py
+++ jobs/models.py
@@ -5,7 +5,7 @@ class Job(models.Model):
     # Job has an image of the job, a summary, and a title
     title = models.CharField(max_length=200)
     image = models.ImageField(upload_to='images/')
-    summary = models.CharField(max_length=200)
+    summary = models.CharField(max_length=500)
 
     def __str__(self):
         return self.title
